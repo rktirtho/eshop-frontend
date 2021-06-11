18438ff7f94bc4a21bf4b70beae61fbbfb83560c
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-06-11 11:54:31 +0600

    file name changed

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/eshop-front-end'));
+app.use(express.static('./dist/eshop-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/eshop-front-end/'}),
+    res.sendFile('index.html', {root: 'dist/eshop-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
